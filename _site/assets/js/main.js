5b531b3eac3ea7949850ef4fc2725ebd70a71cac
Rémi Parmentier <rparmentier@tilt-studio.fr>
2019-06-19 22:54:32 +0200

    Add search (work in progress)

---
+++ _site/assets/js/main.js
@@ -0,0 +1,74 @@
+(function() {
+	window.Caniemail = window.Caniemail || {};
+
+	Caniemail.feature = {
+		init: function() {
+			Caniemail.feature.summary.init();
+		},
+		summary: {
+			init: function() {
+				var summaryElements = document.querySelectorAll('.data-summary-family');
+				for(var i = 0; i < summaryElements.length; i++) {
+					summaryElements[i].addEventListener('click', function(e) {
+						Caniemail.feature.summary.unselect();
+						this.classList.add('selected');
+					});
+				}
+			},
+			unselect: function() {
+				var selected = document.querySelector('.data-summary-family.selected');
+				if(selected) {
+					selected.classList.remove('selected');
+				}
+			}
+		}
+	};
+
+	Caniemail.search = {
+		init: function() {
+			Caniemail.search.initSimpleJekyllSearchPlugin();
+			Caniemail.search.bindFeatureLoadOnKeyUp();
+		},
+		initSimpleJekyllSearchPlugin: function() {
+			var sjs = SimpleJekyllSearch({
+				searchInput: document.getElementById('caniemail-search'),
+				resultsContainer: document.getElementById('caniemail-search-results'),
+				json: '/assets/js/search.json',
+				noResultsText: '<p class="caniemail-search-empty">No results found.</p>',
+				searchResultTemplate: `<section class="feature feature--placeholder" data-url="{url}">
+					<header class="feature-header">
+						<h1 class="feature-title">{title}</h1>
+					</header>
+					<div class="data"></div>
+					<div class="data-details"></div>
+					<footer class="feature-footer"></footer>
+				</section>`,
+				success: Caniemail.search.onJsonLoad
+			});
+			//sjs.search('attribute');
+		},
+		bindFeatureLoadOnKeyUp: function() {
+			var input = document.getElementById('caniemail-search');
+			input.addEventListener('keyup', function(e) {
+				Caniemail.search.onKeyUp(e);
+			});
+		},
+		onJsonLoad: function(json) {
+			console.log('onJsonLoad', json);
+		},
+		onKeyUp: function(e) {
+			setTimeout(function() {
+				var features = document.querySelectorAll('#caniemail-search-results .feature--placeholder');
+				for(var i=0; i < features.length; i++) {
+					console.log(features[i]);
+					features[i].classList.add('foo'+i);
+				}
+			}, 1000);
+		}
+	};
+
+	document.addEventListener('DOMContentLoaded', function() {
+		Caniemail.feature.init();
+		Caniemail.search.init();
+	});
+})();
--- assets/js/caniemail.js
+++ assets/js/caniemail.js
@@ -1,30 +1,5 @@
-(function() {
-	window.Caniemail = window.Caniemail || {};
-
-	Caniemail.feature = {
-		init: function() {
-			Caniemail.feature.summary.init();
-		},
-		summary: {
-			init: function() {
-				var summaryElements = document.querySelectorAll('.data-summary-family');
-				for(var i = 0; i < summaryElements.length; i++) {
-					summaryElements[i].addEventListener('click', function(e) {
-						Caniemail.feature.summary.unselect();
-						this.classList.add('selected');
-					});
-				}
-			},
-			unselect: function() {
-				var selected = document.querySelector('.data-summary-family.selected');
-				if(selected) {
-					selected.classList.remove('selected');
-				}
-			}
-		}
-	};
-
-	document.addEventListener('DOMContentLoaded', function() {
-		Caniemail.feature.init();
-	});
-})();
+---
+layout: null
+---
+{% include_relative main.js %}
+{% include_relative simple-jekyll-search.min.js %}
