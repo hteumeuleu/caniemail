965f86781a16bb557aaee3fff8a3fce3a6a24f2c
Rémi Parmentier <remi@hteumeuleu.fr>
2019-08-23 13:37:36 +0200

    Add cookie to maintain accessible colors option

--- assets/js/_accessible-colors.js
+++ assets/js/_accessible-colors.js
@@ -2,16 +2,65 @@ class AccessibleColors {
 
 	constructor() {
 
+		this.button = document.querySelector('.a11y-colors-button');
+		this.enabled = false;
+		this.cookieName = 'accessible-colors-enabled';
 		this.addEvent();
 	}
 
 	addEvent() {
 
-		const element = document.querySelector('.a11y-colors-button');
-		element.addEventListener('click', e => {
+		this.init();
+
+		this.button.addEventListener('click', e => {
 
 			e.preventDefault();
-			document.body.classList.toggle('accessible-colors-enabled');
+			this.toggle();
 		});
+	}
+
+	init() {
+		const enableAccessibleColorsCookie = this.readCookie();
+		if(enableAccessibleColorsCookie == 'true') {
+			this.toggle();
+		}
+	}
+
+	toggle() {
+
+		this.enabled = !this.enabled;
+		this.toggleCSSClass();
+		this.toggleLabel();
+		this.setCookie();
+	}
+
+	toggleCSSClass() {
+
+		const newClassName = this.cookieName;
+		document.body.classList.toggle(newClassName);
+	}
+
+	toggleLabel() {
+
+		const dataAttributeName = 'data-label-toggle';
+		const oldLabel = this.button.innerText;
+		const newLabel = this.button.getAttribute(dataAttributeName);
+		this.button.innerText = newLabel;
+		this.button.setAttribute(dataAttributeName, oldLabel);
+
+	}
+
+	setCookie() {
+
+        let date = new Date();
+        date.setTime( date.getTime() + (30 * 24 * 60 * 60 * 1000) );
+        const expires = ';expires='+ date.toGMTString();
+		document.cookie = `${this.cookieName}=${this.enabled}${expires};path=/;`;
+	}
+
+	readCookie() {
+
+        let b = document.cookie.match(`(^|;)\\s*${this.cookieName}\\s*=\\s*([^;]+)`);
+        return b ? b.pop() : null;
 	}
 }
