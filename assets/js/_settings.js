53e797bce83b5e73319d79a32e60f93036506527
HTeuMeuLeu <remi@hteumeuleu.fr>
2020-04-04 21:52:37 +0200

    More styles and JS for settings

--- assets/js/_settings.js
+++ assets/js/_settings.js
@@ -8,9 +8,17 @@ class Settings {
 		if(this.panel && this.button) {
 			this.init();
 		}
+
+		this.toggle();
 	}
 
-	addEvent() {
+	init() {
+
+		this.addEventToButton();
+		this.addEventToCheckboxes();
+	}
+
+	addEventToButton() {
 
 		this.button.addEventListener('click', e => {
 
@@ -19,9 +27,46 @@ class Settings {
 		});
 	}
 
-	init() {
-
-		this.addEvent();
+	addEventToCheckboxes() {
+		const checkboxes = this.panel.querySelectorAll('input[type="checkbox"]');
+		checkboxes.forEach(checkbox => {
+			checkbox.addEventListener('click', e => {
+				if(checkbox.parentNode.className == 'caniemail-settings-list-item') {
+					const childCheckboxes = checkbox.parentNode.querySelectorAll('.caniemail-settings-child-list-item input[type="checkbox"]');
+					if(childCheckboxes.length > 0) {
+						const checkboxStatus = checkbox.checked;
+						childCheckboxes.forEach(childCheckbox => {
+							childCheckbox.checked = checkboxStatus;
+						});
+					}
+				} else if(checkbox.parentNode.className == 'caniemail-settings-child-list-item') {
+					const parentCheckbox = checkbox.parentNode.parentNode.parentNode.querySelector('input[type="checkbox"]');
+					const siblingCheckboxes = parentCheckbox.parentNode.querySelectorAll('.caniemail-settings-child-list-item input[type="checkbox"]');
+					if(parentCheckbox.checked == true) {
+						if(siblingCheckboxes.length == 1) {
+							parentCheckbox.indeterminate = false;
+						} else {
+							parentCheckbox.indeterminate = true;
+						}
+						parentCheckbox.checked = false;
+					} else {
+						let checkboxesStatusIsTheSame = true;
+						siblingCheckboxes.forEach(siblingCheckbox => {
+							if(siblingCheckbox.checked != checkbox.checked) {
+								checkboxesStatusIsTheSame = false;
+							}
+						});
+						if(checkboxesStatusIsTheSame) {
+							parentCheckbox.checked = checkbox.checked;
+							parentCheckbox.indeterminate = false;
+						} else {
+							parentCheckbox.checked = false;
+							parentCheckbox.indeterminate = true;
+						}
+					}
+				}
+			});
+		});
 	}
 
 	toggle() {
@@ -29,4 +74,6 @@ class Settings {
 		this.panel.toggleAttribute('hidden');
 		document.body.classList.toggle('settings-opened');
 	}
+
+
 }
